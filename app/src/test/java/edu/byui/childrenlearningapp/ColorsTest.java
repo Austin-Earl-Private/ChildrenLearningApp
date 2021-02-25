07cbf26904ed41237f1f4e06c67a835a58d356df
Austin-Earl-Private <theqmind2020@gmail.com>
2021-02-24 17:21:19 -0700

    added generated tests

---
+++ app/src/test/java/edu/byui/childrenlearningapp/ColorsTest.java
@@ -0,0 +1,36 @@
+package edu.byui.childrenlearningapp;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ColorsTest {
+
+    @Test
+    public void getName() {
+    }
+
+    @Test
+    public void setName() {
+    }
+
+    @Test
+    public void getPrimary() {
+    }
+
+    @Test
+    public void setPrimary() {
+    }
+
+    @Test
+    public void playSound() {
+    }
+
+    @Test
+    public void onCreateDisplay() {
+    }
+
+    @Test
+    public void onPress() {
+    }
+}
