beb9a7a05478dd78c8a553652d3a251eb868068f
jofravimusic <jofravimusic@byui.edu>
2021-03-03 21:59:35 -0300

    Created Static Class and Added a function to MainActivity

--- app/src/main/java/edu/byui/childrenlearningapp/Child.java
+++ app/src/main/java/edu/byui/childrenlearningapp/Child.java
@@ -20,4 +20,12 @@ public class Child {
     public void setAge(int age) {
         this.age = age;
     }
+
+    @Override
+    public String toString() {
+        return "Child{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
