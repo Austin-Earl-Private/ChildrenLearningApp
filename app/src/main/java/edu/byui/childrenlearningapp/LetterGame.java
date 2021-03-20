cbaf0d3c6385d7120b6fb972989783fb25b1df26
jofravimusic <jofravimusic@byui.edu>
2021-03-19 22:42:59 -0300

    Displaying the name of the Kid in the game.

--- app/src/main/java/edu/byui/childrenlearningapp/LetterGame.java
+++ app/src/main/java/edu/byui/childrenlearningapp/LetterGame.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LetterGame extends AppCompatActivity {
 
@@ -20,7 +21,7 @@ public class LetterGame extends AppCompatActivity {
 
         SharedPreferences preferences = getSharedPreferences(MainActivity.PREFERENCES, MODE_PRIVATE);
 
-        String childName = preferences.getString(MainActivity.NAME, null);
+        String childName = preferences.getString(MainActivity.NAME, null).toUpperCase();
 
         LinearLayout letterView = findViewById(R.id.lettersView);
         LayoutInflater adder = LayoutInflater.from(this);
@@ -29,7 +30,91 @@ public class LetterGame extends AppCompatActivity {
             View view = adder.inflate(R.layout.letter_item, letterView, false);
 
             ImageView letterImage = view.findViewById(R.id.letterImageView);
-            letterImage.setImageResource(R.drawable.abc_button);
+            char array_letter = childName.charAt(i);
+            String letter = Character.toString(array_letter);
+            switch (letter){
+                case "A":
+                    letterImage.setImageResource(R.drawable.letters_a);
+                    break;
+                case "B":
+                    letterImage.setImageResource(R.drawable.letters_b);
+                    break;
+                case "C":
+                    letterImage.setImageResource(R.drawable.letters_c);
+                    break;
+                case "D":
+                    letterImage.setImageResource(R.drawable.letters_d);
+                    break;
+                case "E":
+                    letterImage.setImageResource(R.drawable.letters_e);
+                    break;
+                case "F":
+                    letterImage.setImageResource(R.drawable.letters_f);
+                    break;
+                case "G":
+                    letterImage.setImageResource(R.drawable.letters_g);
+                    break;
+                case "H":
+                    letterImage.setImageResource(R.drawable.letters_h);
+                    break;
+                case "I":
+                    letterImage.setImageResource(R.drawable.letters_i);
+                    break;
+                case "J":
+                    letterImage.setImageResource(R.drawable.letters_j);
+                    break;
+                case "K":
+                    letterImage.setImageResource(R.drawable.letters_k);
+                    break;
+                case "L":
+                    letterImage.setImageResource(R.drawable.letters_l);
+                    break;
+                case "M":
+                    letterImage.setImageResource(R.drawable.letters_m);
+                    break;
+                case "N":
+                    letterImage.setImageResource(R.drawable.letters_n);
+                    break;
+                case "O":
+                    letterImage.setImageResource(R.drawable.letters_o);
+                    break;
+                case "P":
+                    letterImage.setImageResource(R.drawable.letters_p);
+                    break;
+                case "Q":
+                    letterImage.setImageResource(R.drawable.letters_q);
+                    break;
+                case "R":
+                    letterImage.setImageResource(R.drawable.letters_r);
+                    break;
+                case "S":
+                    letterImage.setImageResource(R.drawable.letters_s);
+                    break;
+                case "T":
+                    letterImage.setImageResource(R.drawable.letters_t);
+                    break;
+                case "U":
+                    letterImage.setImageResource(R.drawable.letters_u);
+                    break;
+                case "V":
+                    letterImage.setImageResource(R.drawable.letters_v);
+                    break;
+                case "W":
+                    letterImage.setImageResource(R.drawable.letters_w);
+                    break;
+                case "X":
+                    letterImage.setImageResource(R.drawable.letters_x);
+                    break;
+                case "Y":
+                    letterImage.setImageResource(R.drawable.letters_y);
+                    break;
+                case "Z":
+                    letterImage.setImageResource(R.drawable.letters_z);
+                    break;
+                default:
+                    Toast.makeText(this,"Not letter found for: "+letter, Toast.LENGTH_SHORT).show();
+            }
+
 
             letterView.addView(view);
 
